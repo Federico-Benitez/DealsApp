67daadd0ad8b3d772deeb1d2a45a7c9276ca443f
Federico Benitez <fedebenitez1696@gmail.com>
2023-09-23 12:05:24 -0300

    feat: banner image on home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,22 @@
 import getDeals from "@/api/fetchDeals";
 import ProductList from "@/components/list";
 import { SearchProvider } from "@/context/SearchContext";
+import Image from "next/image";
+import banner from "../../public/banner.jpeg";
 
 export default async function Home() {
   const deals = await getDeals();
   return (
     <SearchProvider data={deals}>
+      <div className="relative h-[100px] xs:h-[130px] sm:h-[190px] md:h-[230px] lg:h-[400px]">
+        <Image
+          src={banner}
+          alt="banner"
+          layout="fill"
+          objectFit="cover"
+          objectPosition="top center"
+        />
+      </div>
       <main className="flex justify-center py-10">
         <ProductList />
       </main>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Header() {
   return (
-    <div className="flex sm:justify-between h-20 border-b-2 py-4 px-8 items-center">
+    <div className="flex sm:justify-between h-20 py-4 px-8 items-center">
       <h1 className="pr-5">Deals</h1>
       <nav>
         <a className="px-5">Home</a>
