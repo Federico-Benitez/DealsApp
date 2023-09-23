ad4c09f7ea53b65066a210871ad3fb1636487c05
Federico Benitez <fedebenitez1696@gmail.com>
2023-09-23 11:25:35 -0300

    fix: header responsive

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Header() {
   return (
-    <div className="flex justify-between h-20 border-b-2 py-4 px-8 items-center">
-      <h1>Deals</h1>
+    <div className="flex sm:justify-between h-20 border-b-2 py-4 px-8 items-center">
+      <h1 className="pr-5">Deals</h1>
       <nav>
         <a className="px-5">Home</a>
         <a className="px-5">Browse</a>
