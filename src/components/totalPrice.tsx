2a729501c9e7866d081b42b7a4f8d00f543ca523
Federico Benitez <fedebenitez1696@gmail.com>
2023-09-23 11:22:33 -0300

    fix: responsive on total price

--- src/components/totalPrice.tsx
+++ src/components/totalPrice.tsx
@@ -9,7 +9,7 @@ export default function TotalPriceBanner() {
   if (!price) return null;
 
   return (
-    <div className="py-6 bg-gradient-to-r from-orange-300 from-1%  via-rose-500 via-20% to-fuchsia-800 to-90% sticky bottom-0 flex justify-center align-middle">
+    <div className="py-6 bg-gradient-to-r from-orange-300 from-1%  via-rose-500 via-20% to-fuchsia-800 to-90% sticky bottom-0 md:flex grid justify-center align-middle">
       <div className="flex justify-center">
         <h4 className="text-2xl pr-4">Precio Total</h4>
         <b className="text-2xl">${price.toFixed(2)}</b>
@@ -17,7 +17,7 @@ export default function TotalPriceBanner() {
 
       <button
         onClick={clearItems}
-        className="ml-6 outline outline-1 outline-[#dadada50] rounded-md  px-5 "
+        className="mt-4 md:mt-0 md:ml-6 outline outline-1 outline-[#dadada50] rounded-md  px-5 "
       >
         <span>Limpiar carrito</span>
       </button>
