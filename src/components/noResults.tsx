fd8db76395e6177b02c1978bbd22d0623cd2b1e5
Federico Benitez <fedebenitez1696@gmail.com>
2023-09-23 11:19:25 -0300

    fix: responsive on no results message

--- src/components/noResults.tsx
+++ src/components/noResults.tsx
@@ -6,15 +6,15 @@ export default function NoResultsMessage() {
   const { searchValue, clearSearch } = useSearchContext();
 
   return (
-    <div className="h-128 grid justify-items-center ">
-      <h3 className="text-7xl text-center max-w-6xl">
+    <div className="h-128 grid justify-items-center">
+      <h3 className="text-xl sm:text-2xl md:text-5xl text-center max-w-xs lg:max-w-full">
         {`No se encontraron resultados para '${searchValue}'`}
       </h3>
       <button
         onClick={clearSearch}
-        className="border border-gray-600 rounded-lg p-6 h-24 w-fit"
+        className="border border-gray-600 rounded-lg px-2 md:p-6 h-14 md:h-24 w-fit"
       >
-        <p className="text-2xl">Ver todos los productos</p>
+        <p className="md:text-2xl">Ver todos los productos</p>
       </button>
     </div>
   );
