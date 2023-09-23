40a3b709297368e9ea11aae8e0c870f5f482dc9c
Federico Benitez <fedebenitez1696@gmail.com>
2023-09-23 10:56:00 -0300

    fix: responsive on footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -12,17 +12,17 @@ const links = [
 
 export default function Footer() {
   return (
-    <footer className="flex justify-center py-5 items-center">
+    <footer className="flex justify-center py-5 lg:items-center border-t-2">
       <h1 className="text-2xl mr-10">Deals</h1>
 
-      <nav>
+      <nav className="grid lg:grid-flow-col">
         {links.map((link, i) => (
-          <>
-            {i > 0 && <span>|</span>}
+          <div key={i} className="py-2 lg:py-0">
+            {i > 0 && <span className="hidden lg:inline">|</span>}
             <a className="px-1" key={link}>
               {link}
             </a>
-          </>
+          </div>
         ))}
       </nav>
     </footer>
